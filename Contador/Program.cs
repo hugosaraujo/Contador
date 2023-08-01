42a24d2c559db77b44ebbab97d39f99b1f43f643
hugo de araujo <hugo.saraujo@tutanota.com>
2023-07-31 21:16:03 -0300

    Adicionar arquivos de projeto.

---
+++ Contador/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> listaDeInteiros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//for  (int i = 0; i < listaDeInteiros.Count; i++)
+//{
+//    if(i % 2 == 0)
+//    {
+//        Console.WriteLine(listaDeInteiros[i]);
+//    }
+//}
+
+foreach (int numeros in listaDeInteiros)
+{
+    if (numeros % 2 == 0)
+    {
+        Console.WriteLine(numeros);
+    }
+}
