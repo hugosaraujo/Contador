cd36637a34d1eef8f16f7eb471c6a851a4cdc643
hugo de araujo <hugo.saraujo@tutanota.com>
2023-09-29 19:02:37 -0300

    criação de loop de contagem utilizando foreach e for

--- Contador/Program.cs
+++ Contador/Program.cs
@@ -1,17 +1,24 @@
-﻿List<int> listaDeInteiros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+﻿List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-//for  (int i = 0; i < listaDeInteiros.Count; i++)
-//{
-//    if(i % 2 == 0)
-//    {
-//        Console.WriteLine(listaDeInteiros[i]);
-//    }
-//}
-
-foreach (int numeros in listaDeInteiros)
+foreach (int numero in numeros)
 {
-    if (numeros % 2 == 0)
+    if (numero % 2 != 0)
     {
-        Console.Write(numeros);
+        Console.Write($"{numero} ");
+    }
+}
+
+//contagem feita com for
+List<int> listaDeNumeros = new List<int>();
+
+for (int numero = 0; numero < 11; numero++)
+{
+    listaDeNumeros.Add(numero);
+    for (int i = numero; i < listaDeNumeros.Count; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine(listaDeNumeros[numero]);
+        }
     }
 }
